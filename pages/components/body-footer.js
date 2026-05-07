fff4a3f3961e9e2e5a70b240ede2128a50ec4cc5
Squadsantuy <Squadsantuy272@gmail.com>
2026-05-07 18:51:52 +0700

    Update footer links to correct URLs

--- pages/components/body-footer.js
+++ pages/components/body-footer.js
@@ -4,13 +4,13 @@ body_footer.insertAdjacentHTML('afterbegin', `
     <ul class="left-panel list-inline mb-0 p-0">
       <li class="list-inline-item">
         <img src="../../assets/logo altie putih.png" height="20px" alt="Logo Altie" />
-        <a href="https://hidayah.sambong.web.id/">HIDAYAH-SAMBONG</a>
+        <a href="./dasboard.html">HIDAYAH</a>
       </li>
     </ul>
     <div class="right-panel">
       Copyright ©
         ${new Date().getFullYear()}
-       by <a href="./dashboard.html">sambong.web.id</a>.
+       by <a href="https://sambong.web.id/">sambong.web.id</a>.
     </div>
   </div>
 `);
